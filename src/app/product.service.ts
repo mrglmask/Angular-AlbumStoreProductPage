cf4bd9598cd48f0154bdb907d4681a2a89e576e0
mrglmask <jaimejcastillo@gmail.com>
2018-07-25 09:22:49 -0600

    album info is dincamically passed through a service

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { Response } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 ///add/operator/map
 @Injectable()
 export class ProductService {
   private _albumUrl: string = '../assets/album.json';
-  
+
   //injecting an instance of a service into a class.
   constructor(private _http: Http) {
     
